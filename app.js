c9f308bd9b18bfbd88c712a97709de027a88a885
Gaddiel <gpatino0@ucol.mx>
2020-12-13 16:38:20 -0600

    commit2

--- app.js
+++ app.js
@@ -3,18 +3,15 @@ if ('serviceWorker' in navigator){
       .then(reg => console.log('Registro de SW exitoso', reg))
       .catch(err => console.warn('Error al tratar de registrar el sw', err))
   }
-
 let screen = document.querySelector('#screen-result');
 let screenM = document.querySelector('#screen-result-memo');
 let  getData = (ref) => {
     let value = ref.value;
     screen.value += value;
 }
-
 let limpiar = () => {
     screen.value = '';
 }
-
 let resultado = () => {
     try{
         screen.value = eval(screen.value);
@@ -26,7 +23,35 @@ let resultado = () => {
         },500);
     }
 }
+//añade memoria 
 let M = () => {
-   
-  
+    try{
+        screen.value = eval(screen.value);
+
+    }catch(error){
+        screen.value = 'error';
+        setTimeout(() => {
+            clean();
+        },500);
+    }
+    screen.value = eval(screen.value);
+    screenM.value = screen.value;
 }
+//limpia memoria 
+let MR = () => {
+    screenM.value = '';
+}
+//copia memoria
+let MC = () => {
+    if(screenM.value==="0"){
+    }else{
+
+      screen.value = screen.value  + screenM.value;
+    }
+  }
+
+  
+
+ 
+
+  
