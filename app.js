efb20ef99a01e867c642bd5685588bdeacf209a5
Ulises Rafael Ramirez Martinez <vwalle.r.m@gmail.com>
2020-12-20 16:10:05 -0600

    firt commit

--- app.js
+++ app.js
@@ -1,57 +1,140 @@
-if ('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw.js')
-      .then(reg => console.log('Registro de SW exitoso', reg))
-      .catch(err => console.warn('Error al tratar de registrar el sw', err))
-  }
-let screen = document.querySelector('#screen-result');
-let screenM = document.querySelector('#screen-result-memo');
-let  getData = (ref) => {
-    let value = ref.value;
-    screen.value += value;
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./service-worker.js')
+    .then((reg) => console.log('Registro de SW exitoso', reg))
+    .catch((err) => console.warn('Error al tratar de registrar el sw', err));
 }
-let limpiar = () => {
-    screen.value = '';
-}
-let resultado = () => {
-    try{
-        screen.value = eval(screen.value);
 
-    }catch(error){
-        screen.value = 'error';
-        setTimeout(() => {
-            clean();
-        },500);
+var operandoa;
+var operandob;
+var operacion;
+
+function init() {
+  var resultado = document.getElementById('resultado');
+  var memoria = document.getElementById('memoriadisplay');
+  var reset = document.getElementById('reset');
+  var suma = document.getElementById('suma');
+  var resta = document.getElementById('resta');
+  var multiplicacion = document.getElementById('multiplicacion');
+  var division = document.getElementById('division');
+  var igual = document.getElementById('igual');
+  var uno = document.getElementById('uno');
+  var dos = document.getElementById('dos');
+  var tres = document.getElementById('tres');
+  var cuatro = document.getElementById('cuatro');
+  var cinco = document.getElementById('cinco');
+  var seis = document.getElementById('seis');
+  var siete = document.getElementById('siete');
+  var ocho = document.getElementById('ocho');
+  var nueve = document.getElementById('nueve');
+  var cero = document.getElementById('cero');
+  var agregarm = document.getElementById('agregarmemoria');
+  var traerm = document.getElementById('traermemoria');
+  var limpiarm = document.getElementById('limpiarmemoria');
+
+  uno.onclick = function (e) {
+    if (resultado.value == 0) {
+      limpiar();
     }
-}
-//añade memoria 
-let M = () => {
-    try{
-        screen.value = eval(screen.value);
-
-    }catch(error){
-        screen.value = 'error';
-        setTimeout(() => {
-            clean();
-        },500);
+    resultado.value = resultado.value + '1';
+  };
+  dos.onclick = function (e) {
+    if (resultado.value == 0) {
+      limpiar();
     }
-    screen.value = eval(screen.value);
-    screenM.value = screen.value;
-}
-//limpia memoria 
-let MR = () => {
-    screenM.value = '';
-}
-//copia memoria
-let MC = () => {
-    if(screenM.value==="0"){
-    }else{
-
-      screen.value = screen.value  + screenM.value;
+    resultado.value = resultado.value + '2';
+  };
+  tres.onclick = function (e) {
+    if (resultado.value == 0) {
+      limpiar();
     }
-  }
+    resultado.value = resultado.value + '3';
+  };
+  cuatro.onclick = function (e) {
+    if (resultado.value == 0) {
+      limpiar();
+    }
+    resultado.value = resultado.value + '4';
+  };
+  cinco.onclick = function (e) {
+    if (resultado.value == 0) {
+      limpiar();
+    }
+    resultado.value = resultado.value + '5';
+  };
+  seis.onclick = function (e) {
+    if (resultado.value == 0) {
+      limpiar();
+    }
+    resultado.value = resultado.value + '6';
+  };
+  siete.onclick = function (e) {
+    if (resultado.value == 0) {
+      limpiar();
+    }
+    resultado.value = resultado.value + '7';
+  };
+  ocho.onclick = function (e) {
+    if (resultado.value == 0) {
+      limpiar();
+    }
+    resultado.value = resultado.value + '8';
+  };
+  nueve.onclick = function (e) {
+    if (resultado.value == 0) {
+      limpiar();
+    }
+    resultado.value = resultado.value + '9';
+  };
+  cero.onclick = function (e) {
+    if (resultado.value == 0) {
+      limpiar();
+    }
+    resultado.value = resultado.value + '0';
+  };
+  punto.onclick = function (e) {
+    resultado.value = resultado.value + '.';
+  };
+  suma.onclick = function (e) {
+    resultado.value = resultado.value + '+';
+  };
+  resta.onclick = function (e) {
+    resultado.value = resultado.value + '-';
+  };
+  multiplicacion.onclick = function (e) {
+    resultado.value = resultado.value + '*';
+  };
+  division.onclick = function (e) {
+    resultado.value = resultado.value + '/';
+  };
+  reset.onclick = function (e) {
+    resetear();
+  };
+  igual.onclick = function (e) {
+    resultado.value = eval(resultado.value);
+  };
+  agregarm.onclick = function (e) {
+    resultado.value = eval(resultado.value);
+    memoria.value = eval(resultado.value);
+  };
+  traerm.onclick = function (e) {
+    if (memoria.value === 'M') {
+    } else {
+      resultado.value = resultado.value + memoria.value;
+    }
+  };
+  limpiarm.onclick = function (e) {
+    memoria.value = 'M';
+  };
+}
 
-  
+function limpiar() {
+  resultado.value = '';
+}
 
- 
-
-  
+function resetear() {
+  resultado.value = '';
+  operandoa = 0;
+  operandob = 0;
+  operacion = '';
+}
--- sw.js
+++ sw.js
@@ -1,59 +1,58 @@
-const cache_name = 'cache2',
-urlsToCache = [
+const cache_name = 'cache3',
+  urlsToCache = [
     './',
     'estilo.css',
     'app.js',
-    'android-chrome-192x192.png',
-    'android-chrome-512x512.png',
-    'favicon-32x32.png',
-    'favicon-16x16.png',
-    
-]
+    'favicon_io/android-chrome-192x192.png',
+    'favicon_io/android-chrome-512x512.png',
+    'favicon_io/favicon-32x32.png',
+    'favicon_io/favicon-16x16.png',
+  ];
 
 //para funcionar sin conexion
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(cache_name)
-          .then(cache => {
-            return cache.addAll(urlsToCache)
-              .then(() => self.skipWaiting())
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches
+      .open(cache_name)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => self.skipWaiting());
+      })
+      .catch((err) => console.log('Falló registro de cache', err))
+  );
+});
+//para funcionar sin conexion
+self.addEventListener('activate', (e) => {
+  const cacheWhitelist = [cache_name];
+
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
           })
-          .catch(err => console.log('Falló registro de cache', err))
-      )
-    })
-//para funcionar sin conexion
-self.addEventListener('activate', e => {
-    const cacheWhitelist = [cache_name]
-
-    e.waitUntil(
-      caches.keys()
-        .then(cacheNames => {
-          return Promise.all(
-            cacheNames.map(cacheName => {
-              //Eliminamos lo que ya no se necesita en cache
-              if (cacheWhitelist.indexOf(cacheName) === -1) {
-                return caches.delete(cacheName)
-              }
-            })
-          )
-        })
-        // Le indica al SW activar el cache actual
-        .then(() => self.clients.claim())
-    )
-  })
+        );
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  );
+});
 
 //recuperae archivos
-self.addEventListener('fetch', e => {
- //Responder ya sea con el objeto en caché o continuar y buscar la url real
- e.respondWith(
-    caches.match(e.request)
-      .then(res => {
-        if (res) {
-          //recuperar del cache
-          return res
-        }
-        //recuperar de la petición a la url
-        return fetch(e.request)
-      })
-  )
-})
+self.addEventListener('fetch', (e) => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        //recuperar del cache
+        return res;
+      }
+      //recuperar de la petición a la url
+      return fetch(e.request);
+    })
+  );
+});
